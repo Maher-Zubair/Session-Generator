8768843ab98a6c5323bfea0d344a755c92c4da27
ɪɴɴᴏxᴇɴᴛ ᴢᴜʙɪ <143280418+Maher-Zubair@users.noreply.github.com>
2024-02-29 06:19:09 +0000

    sᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ ❣

--- pair.js
+++ pair.js
@@ -54,14 +54,14 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(user, { text: 'SIGMA-MD;;;' + b64data });
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'SIGMA-MD;;;' + b64data });
 
                let SIGMA_MD_TEXT = `
 *_Pair Code By Maher Zubair_*
 *_Made With 🤍_*
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Maher_Zubair.sendMessage(user,{text:SIGMA_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
  
 
         await delay(100);
--- qr.js
+++ qr.js
@@ -56,14 +56,14 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(user, { text: 'SIGMA-MD;;;' + b64data });
+				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: 'SIGMA-MD;;;' + b64data });
 	
 				   let SIGMA_MD_TEXT = `
-	*_Pair Code By Maher Zubair_*
+	*_Qr Code By Maher Zubair_*
 	*_Made With 🤍_*
 	
 	_Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Maher_Zubair.sendMessage(user,{text:SIGMA_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
 
 
 
